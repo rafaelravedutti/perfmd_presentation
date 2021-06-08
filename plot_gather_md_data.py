fbdb745efab0103f12bdca275cdf35d8780e3122
Rafael Ravedutti <rafaelravedutti@gmail.com>
2021-06-08 03:07:44 +0200

    Update slides (hopefully last version)
    Signed-off-by: Rafael Ravedutti <rafaelravedutti@gmail.com>

--- plot_gather_md_data.py
+++ plot_gather_md_data.py
@@ -11,17 +11,18 @@ plot_data = {}
 
 with open(filename, 'r') as fp:
     for line in fp.readlines():
-        stride, N, size, total_time, time_per_it, cy_per_gather, cy_per_elem  = line.split(',')
+        stride, N, size, total_time, time_per_it, cy_per_it, cy_per_gather, cy_per_elem  = line.split(',')
         stride = int(stride)
         size = float(size)
-        cy_per_gather = float(cy_per_gather)
+        cy_per_it = float(cy_per_it)
 
-        if stride < 8:
+        #if stride < 5 and stride != 3:
+        if stride < 100:
             if stride not in plot_data:
                 plot_data[stride] = {}
 
-            plot_data[stride][size] = cy_per_gather if size not in plot_data[stride] \
-                                      else min(cy_per_gather, plot_data[stride][size])
+            plot_data[stride][size] = cy_per_it if size not in plot_data[stride] \
+                                      else min(cy_per_it, plot_data[stride][size])
 
 for stride in plot_data:
     sizes = list(plot_data[stride].keys())
@@ -35,5 +36,6 @@ ax.set(xlabel='Array size (kB)', ylabel='Cycles per gather')
 ax.set_xscale('log')
 #ax.set_xticks([32, 1000, 28000])
 #ax.set_xlim(0, 200000)
+#ax.set_ylim(25, 60)
 plt.legend(title="Stride")
 fig.savefig(output_file, bbox_inches = 'tight', pad_inches = 0)
